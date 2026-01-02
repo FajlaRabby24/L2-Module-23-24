2de2f64c1e8dbf1b25b2ddbc5eb63f623bff1ae5
Fajla Rabby <fajlarabbyla24@gmail.com>
2026-01-02 01:21:09 -0500

    23-9 Implementing Create Post API - Part 1

---
+++ modules/post/post.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+const createPost = async (req: Request, res: Response) => {
+  res.send("Create a new post");
+};
+export const PostController = {
+  createPost,
+};
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
+import { postRouter } from "../modules/post/post.router";
 
 const app = express();
+
+app.use("/post", postRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world");
