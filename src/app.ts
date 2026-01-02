e8ade87daca9467909f834ddf45e696cf8ccfa8b
Fajla Rabby <fajlarabbyla24@gmail.com>
2026-01-02 11:49:12 -0500

    24-1 Introduction to email & password authentication (better auth setup)

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
+import { toNodeHandler } from "better-auth/node";
 import express, { Request, Response } from "express";
+import { auth } from "./lib/auth";
 import { postRouter } from "./modules/post/post.router";
 
 const app = express();
 
 app.use(express.json());
+app.all("/api/auth/*spalte", toNodeHandler(auth));
 
 app.use("/post", postRouter);
 
